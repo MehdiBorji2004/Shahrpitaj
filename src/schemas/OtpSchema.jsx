9be58f1dc32a0337653d299c2f66011a6b2092d0
borjim7 <mehdisystem83@gmail.com>
2025-12-15 22:25:16 +0330

    edit otp schema in frontend

--- src/schemas/OtpSchema.jsx
+++ src/schemas/OtpSchema.jsx
@@ -2,8 +2,8 @@ import * as Yup from "yup";
 
 const otpSchema = Yup.object().shape({
   otp: Yup.string()
-    .min(4, "کد تایید باید حداقل ۴ رقم باشد*")
-    .max(4, "کد تایید باید ۴ رقم باشد*")
+    .min(6, "کد تایید باید حداقل 6 رقم باشد*")
+    .max(6, "کد تایید باید 6 رقم باشد*")
     .required("کد تایید الزامی است*"),
 });
 
