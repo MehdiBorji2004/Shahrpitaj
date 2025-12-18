8ae5ceec188be575df751a4d17fa077debc00119
borjim7 <mehdisystem83@gmail.com>
2025-12-18 14:04:46 +0330

    edit send verify sms

--- utils/sendVerifyCode.js
+++ utils/sendVerifyCode.js
@@ -6,16 +6,14 @@ const sendVerifyCode = async (mobile, code) => {
     "https://api.sms.ir/v1/send/verify",
     {
       mobile,
-      templateId: process.env.SMSIR_TEMPLATE_ID || Number(750316),
+      templateId: process.env.SMSIR_TEMPLATE_ID,
       parameters: [{ name: "CODE", value: code }],
     },
     {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
-        "x-api-key":
-          process.env.SMSIR_API_KEY ||
-          "n9U1qClMNYViLL2qqbtxCpn41s1TcKIyqZOls2vT1WLzismD",
+        "x-api-key": process.env.SMSIR_API_KEY,
       },
     }
   );
